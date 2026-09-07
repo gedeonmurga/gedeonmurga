3c6d5ba115017b0ffca32e0ae8d80e152db586e9
gedeonmurga <abadmurga565@gmail.com>
2026-09-07 10:57:13 -0500

    Avancede la clase CRUD1

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/exception/ModelNotFoundExeption.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.sysventas.exception;
+
+public class ModelNotFoundExeption extends  RuntimeException{
+    public ModelNotFoundExeption(String message) {
+        super(message);
+    }
+}
